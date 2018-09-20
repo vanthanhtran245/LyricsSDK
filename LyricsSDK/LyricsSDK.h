cece12fde2c2301047cea0e053d100c3c9ccacbc
Tran Van Thanh <thanhtran.hce@gmail.com>
2018-09-20 15:07:14 +0700

    Initial Commit

---
+++ LyricsSDK/LyricsSDK.h
@@ -0,0 +1,19 @@
+//
+//  LyricsSDK.h
+//  LyricsSDK
+//
+//  Created by Thanh Tran Van on 9/20/18.
+//  Copyright © 2018 Thanh Tran Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LyricsSDK.
+FOUNDATION_EXPORT double LyricsSDKVersionNumber;
+
+//! Project version string for LyricsSDK.
+FOUNDATION_EXPORT const unsigned char LyricsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LyricsSDK/PublicHeader.h>
+
+
